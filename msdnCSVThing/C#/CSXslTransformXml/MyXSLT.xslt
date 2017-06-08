19ce34d426ec1f020fe6e7f52b5e691161048315
EasternCatNZL <brianpwwong@gmail.com>
2017-06-08 16:28:34 +1200

    Yeah baby it works Fuck c++

---
+++ msdnCSVThing/C#/CSXslTransformXml/MyXSLT.xslt
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
+>
+  <xsl:output method="text"/>
+
+  <xsl:template match="catalog">
+    <xsl:apply-templates select ="game"/>
+  </xsl:template>
+
+  <xsl:template match="game">
+    <xsl:text>&#34;</xsl:text>
+    <xsl:for-each select ="*">
+      <xsl:text>&#34;</xsl:text>
+      <xsl:value-of select ="."/>
+      <xsl:text>&#34;</xsl:text>
+      <xsl:if test ="position() != last()">
+        <xsl:value-of select ="','"/>
+      </xsl:if>
+    </xsl:for-each>
+    <xsl:text>&#xD;&#xa;</xsl:text>
+  </xsl:template>
+</xsl:stylesheet>
---
+++ msdnCSVThing/C#/CSXslTransformXml/Program.cs
@@ -0,0 +1,53 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:  Program.cs
+* Project:      CSXslTransformXml
+* Copyright (c) Microsoft Corporation.
+* 
+* This sample project shows how to use XslCompiledTransform to transform an 
+* XML data file to .csv file using an XSLT style sheet.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+#region Using directives
+using System.Xml.Xsl;
+using System;
+#endregion
+
+
+namespace CSXslTransformXml
+{
+    class Program
+    {
+        private static string xmlFileName;
+        private static string xsltFileName;
+        private static string csvFileName;
+
+        static void Main(string[] args)
+        {
+            //Have console prompt user for files
+            Console.WriteLine("Enter xml filename (Exp: MyData.xml)");
+            xmlFileName = Console.ReadLine();
+            Console.WriteLine("Enter xslt filename(Exp: MyStyle.xslt)");
+            xsltFileName = Console.ReadLine();
+            Console.WriteLine("Enter desired outputted filename(Exp: MyNewFile.csv)");
+            csvFileName = Console.ReadLine();
+
+            // Initialize an XslCompiledTransform instance.
+            XslCompiledTransform transform = new XslCompiledTransform();
+
+            // Call transform.Load() to load the XSLT style sheet Books.xslt.
+            transform.Load(xsltFileName);
+
+            // Call transform.Transform() to transform the source XML file 
+            // to a csv file using the XSLT style sheet.
+            transform.Transform(xmlFileName, csvFileName);
+        }
+    }
+}
